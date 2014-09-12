220e0f5403cba58904bfd95e7074a3aa7634d9ed
Saulo Ricci <infsaulok@gmail.com>
2014-09-12 10:28:52 -0300

    Creating the item to be crawled

--- blockchaininfo_crawler/items.py
+++ blockchaininfo_crawler/items.py
@@ -1,11 +1,8 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 from scrapy.item import Item, Field
 
 class BlockchaininfoCrawlerItem(Item):
-    # define the fields for your item here like:
-    # name = Field()
-    pass
+
+    address = Field()
+    tag = Field()
+    url = Field()
+    verified = Field()
