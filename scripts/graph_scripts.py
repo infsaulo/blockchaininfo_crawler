8b909b164e7028fcc525f78600c4e531d1517187
Saulo Ricci <infsaulok@gmail.com>
2014-12-11 19:07:29 -0200

    Adding gauss-jacobi method to calculate centrality over bitcoin nodes

--- scripts/graph_scripts.py
+++ scripts/graph_scripts.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import argparse
+import numpy
 
 def get_major_strongly_component(graph):
 
@@ -15,6 +16,43 @@ def get_major_strongly_component(graph):
 def output_in_file(object, output_filename):
     nx.write_gpickle(object, output_filename)
 
+def gauss_jacobi_iterate(graph, nodes_list, X_last):
+    
+    X = numpy.asmatrix(numpy.empty((X_last.shape[0], 1)))
+    for index in xrange(X.shape[0]):
+        print "Getting k predecessors of " + str(index) 
+        list_dict_adjacencies = graph.predecessors(nodes_list[index])
+        print "Got k predecessors of " + str(index)
+        value = 0.0
+        for node in list_dict_adjacencies:
+            node_index = nodes_list.index(node)
+            value += (1.0/len(list_dict_adjacencies)) * X_last[node_index, 0]
+        X[index] = value
+    
+    return X
+
+def gauss_jacobi_method(graph, min_quadratic_error):
+    
+    nodes_list = graph.nodes()
+    X_vector = numpy.empty((len(graph.nodes()), 1))
+    X_vector.fill(1.0/len(graph.nodes()))
+    X_last = numpy.asmatrix(X_vector)
+    
+    X = gauss_jacobi_iterate(graph, nodes_list,  X_last)
+    counter = 1
+    while(numpy.linealg.norm(X - X_last) > min_quadratic_error):
+        print "Entered Iteration " + str(counter)
+        X_last = X
+        X = gauss_jacobi_iterate(graph, nodes_list, X_last)
+        print "Finished Iteration " + str(counter)
+        counter += 1
+
+    gauss_jacobi_scores = dict()
+    for index in xrange(X.shape[0]):
+        gauss_jacobi_score[index] = {'user_id': nodes_list[index], 'score': X[index, 0]}
+
+    return gauss_jacobi_score
+ 
 def main():
 
     parser = argparse.ArgumentParser(description='Generate some metrics and graph-related objects to bitcoin network')
@@ -22,13 +60,22 @@ def main():
     parser.add_argument("--is_file_put", dest='is_out_file')
     parser.add_argument("--input_file", dest="input_file")
     parser.add_argument("--output_file", dest="output_file")
+    parser.add_argument("--min_error", dest="min_quadratic_error")
     args = parser.parse_args()
 
+    print "Loading graph"
+    graph = nx.read_gpickle(args.input_file)
+    print "Loaded graph"
     if args.script == 'major_strongly_component' :
-        graph = nx.read_gpickle(args.input_file)
         subgraph = get_major_strongly_component(graph)
         if bool(args.is_out_file):
             output_in_file(subgraph, args.output_file)
+    
+    if args.script == 'gauss_jacobi':
+        score_dict = gauss_jacobi_method(graph, float(args.min_quadratic_error))
+        with open(args.output_file) as out_file:
+            for item in sorted(score_dict.items(), key=lambda (k,v): v['score'], reverse=True):
+                out.write(str(item[0]) + ' ' + item[1]['user_id'] + ' ' + str(item[1]['score']) + '\n')
 
 if __name__ == '__main__':
     main()
--- scripts/transform_adjacency.py
+++ scripts/transform_adjacency.py
@@ -4,35 +4,38 @@ import networkx as nx
 
 def transform_adjacency(graph):
     nodes_list = graph.nodes()
-    adjacency_matrix = numpy.zeros((len(graph.nodes()), len(graph.nodes())))
+    list_edges = []
 
     # Fill the adjacency matrix with the 1, as a default metric in PageRank
     for node in nodes_list:
         node_index = nodes_list.index(node)
         edges_dict = graph[node]
-        factor = 1
         for node2, edge_params in edges_dict.items():
-            node2_index = self.nodes_list.index(node2)
-            adjacency_matrix[node2_index][node_index] = factor
+            node2_index = nodes_list.index(node2)
+            list_edges.append((node_index, node2_index))
 
-    return numpy.asmatrix(adjacency_matrix)
+    return list_edges, nodes_list
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--graph_filename', dest='graph_filename')
     parser.add_argument('--out', dest='out_filename')
+    parser.add_argument('--dictout', dest='dict_out')
     args = parser.parse_args()
 
     graph = nx.read_gpickle(args.graph_filename)
-    matrix = transform_adjacency(graph)
+    list_edges, nodes_list = transform_adjacency(graph)
 
     with open(args.out_filename) as outfile:
-        for index_row in xrange(matrix.shape[0]):
-            for index_column in xrange(matrix.shape[1]):
-                out.write(str(matrix.item(index_row, index_column)) + " ")
+        out.write("M = [\n")
+	for edge in list_edges:
+            out.write(str(edge[0]) + ' ' + str(edge[1]) + '\n')
+	out.write('];')
 
-            out.write("\n")
+    with open(args.dict_out) as dictfile:
+        for index in xrange(len(nodes_list)):
+            dictfile.write(str(index) + ' ' + str(nodes_list[index]) + '\n')
 
 if __name__ == "__main__":
     main()
